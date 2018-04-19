b8918c0adbb8e4146b38f9d1e0cc625c8af05f3e
Jake Ryu <jake.ryu@hotmail.co.uk>
2018-04-19 22:46:25 +0100

    Initial commit

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+export default function () {
+  return [
+    {title: 'Java'},
+    {title: 'Harry Porter'},
+    {title: 'The Dark Tower'},
+    {title: 'Small Step'},
+    {title: 'Ruby'}
+  ];
+}
