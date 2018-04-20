2fca04aeb2a6692319937e2fed9f5473fb69a29b
Jake Ryu <jake.ryu@hotmail.co.uk>
2018-04-20 12:30:03 +0100

    Add select book action and its reducer

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -1,10 +1,17 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
+import {selectBook} from '../actions/index';
+import {bindActionCreators} from 'redux';
 
 class BookList extends Component {
   renderList() {
     return this.props.books.map(book => {
-      return (<li key={book.title} className="list-group-item">{book.title}</li>);
+      return (<li
+                key={book.title}
+                onClick={this.props.selectBook(book)}
+                className="list-group-item">
+                {book.title}
+              </li>);
     });
   }
 
@@ -20,5 +27,12 @@ function mapStateToProps(state) {
   return {books: state.books};
 }
 
+// Anything returned from this function will end up as mapStateToProps
+// on the BookList container
+function mapDispatchToProps(dispatch) {
+  // When selectBook is called, the result should be passed to all of reducer_books.
+  // 'dispatch' takes actions and flow them through reducers
+  return bindActionCreators({ selectBook: selectBook}, dispatch);
+}
 // return a container (smart component)
-export default connect(mapStateToProps)(BookList);
+export default connect(mapStateToProps, mapDispatchToProps)(BookList);
